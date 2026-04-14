7b0ef9b6558025553600f4913c51bf33983451f4
mubasshir <mubasshirkamali2009@gmail.com>
2026-04-14 15:38:45 +0600

    active higlighter done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
-
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
- <div>
-  <h1 className="text-sky-500 bg-white text-9xl text-center">hello world</h1>
- </div>
-  ); 
+  redirect('/home');
 }
+
+
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,37 +1,49 @@
+'use client';
 import React from 'react';
-import logoimg from "../../assets/img/logo.png"
+import logoimg from "../../assets/img/logo.png";
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { IoHomeOutline } from "react-icons/io5";
 import { FaHistory } from "react-icons/fa";
 import { MdOutlineQueryStats } from "react-icons/md";
 
-
 const Navbar = () => {
+  const pathname = usePathname();
 
-  const links=<>
- <li><Link href="/home"><IoHomeOutline />Dashboard</Link></li>
- <li><Link href="/timeline"><FaHistory />
-Timeline</Link></li>
- <li><Link href="/starts"><MdOutlineQueryStats />
-Stats</Link></li>
+  const linkClass = (path) =>
+    pathname === path
+      ? 'text-white font-bold rounded-xl bg-[#244D3F]'
+      : 'text-gray-600 hover:text-teal-600 hover:font-bold';
 
-</>
-
-    return (
-        <div>
-            <div className="navbar bg-base-100 shadow-sm">
-  <div className="flex-1">
-  <Image src={logoimg} alt="keeplogo" />
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      {links}
-    </ul>
-  </div>
-</div>
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <Image src={logoimg} alt="keeplogo"  />
         </div>
-    );
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1 gap-5">
+            <li>
+              <Link href="/home" className={linkClass('/home')}>
+                <IoHomeOutline /> Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/timeline" className={linkClass('/timeline')}>
+                <FaHistory /> Timeline
+              </Link>
+            </li>
+            <li>
+              <Link href="/starts" className={linkClass('/starts')}>
+                <MdOutlineQueryStats /> Stats
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
